f057036e9d7c826727a70ea6e97e3eb90b5234a2
max-davidson823 <maxdavidson823@gmail.com>
2024-08-20 12:41:30 -0500

    refactor: Update SafeAreaView in App component

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">TaskFlow!</Text>
-      <StatusBar style="auto" />
-      <Link href="/boards" style={{ color: 'blue' }}>Go to Boards</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+
+    </SafeAreaView>
   );
 }
